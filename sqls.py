8756cec93ec1ca4729115dc4e111849b956ac1f2
Udaya31 <udaya91221@gmail.com>
2022-10-11 14:32:54 +0530

    try

--- sqls.py
+++ sqls.py
@@ -2,37 +2,38 @@ import mysql.connector
 from sqlalchemy import create_engine
 import pandas as pd
 
-# mydb=mysql.connector.connect(host='localhost',user='root',password='SQL180822udemy',database='vscode')
-# c=mydb.cursor()
-# c.execute("drop table if exists lite")
-# c.execute('''create table lite(application varchar(20),dates datetime,amount float,load_date varchar(20))''')
-# sqs=pd.read_csv('lake1.txt')
-# sqs.to_sql('lite',mydb,index=False,if_exists='replace')
-# # c.execute('''select * from lite''')
-# # for row in c:
-# #     print(row)
-# print(c.fetchall())
-# c.commit()
-# c.close()
-
-hostname="localhost"
-dbname="vscode"
-uname="root"
-pwd="SQL180822udemy"
-
-engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}"
-				.format(host=hostname, db=dbname, user=uname, pw=pwd))
-c=engine.connect()
+mydb=mysql.connector.connect(host='localhost',user='root',password='SQL180822udemy',database='vscode')
+c=mydb.cursor()
 c.execute("drop table if exists lite")
-c.execute('''create table lite(application varchar(20),dates datetime,amount float,load_date datetime)''')
-def insert_d(file):
-	df=pd.read_csv(file)
-	df.to_sql('lite', engine, index=False,if_exists='replace')
-	print('File uploaded to MYSql')
-	# cursor = c.cursor()
-	# query = "SELECT * from history_table"
-	# cursor.execute(query)
-	# cursor.fetchall()
+c.execute('''create table lite(application varchar(20),dates datetime,amount float,load_date date)''')
+sqs=pd.read_csv('lake1.txt')
+sqs.to_sql('lite',mydb,index=False,if_exists='replace')
+# c.execute('''select * from lite''')
+# for row in c:
+#     print(row)
+print(c.fetchall())
+c.commit()
 c.close()
+
+
+# hostname="localhost"
+# dbname="vscode"
+# uname="root"
+# pwd="SQL180822udemy"
+
+# engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}"
+# 				.format(host=hostname, db=dbname, user=uname, pw=pwd))
+# c=engine.connect()
+# c.execute("drop table if exists lite")
+# c.execute('''create table lite(application varchar(20),dates datetime,amount float,load_date datetime)''')
+# def insert_d(file):
+# 	df=pd.read_csv(file)
+# 	df.to_sql('lite', engine, index=False,if_exists='replace')
+# 	print('File uploaded to MYSql')
+# 	# cursor = c.cursor()
+# 	# query = "SELECT * from history_table"
+# 	# cursor.execute(query)
+# 	# cursor.fetchall()
+# c.close()
 
 
